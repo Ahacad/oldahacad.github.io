69de9f982ee69314bfaecac5889e077756edfa5c
ahacad <1393345075@qq.com>
2021-01-13 22:06:09 +0800

    updated icons for sidebar

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -1,13 +1,13 @@
 import React from "react"
 import "./sidebar.css"
 
-import willjw3 from "../../images/willjw3.jpg"
+import ahacad from "../../images/ahacad.jpg"
 
 const Bio = ({ author, tagline }) => {
 
     return (
         <div className="bio-main w-75">
-            <img src={willjw3} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
+            <img src={ahacad} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
             <h3 className="mt-2 author-bio">{author}</h3>
             <small className="text-muted">{tagline}</small>
         </div>
