ffe84baae9389b1e8f61c69a018c94bfcae4c08c
ahacad <1393345075@qq.com>
2021-01-23 22:18:29 +0800

    card now accepts width and height

--- src/pages/card.js
+++ src/pages/card.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-const Card = ({name, description, image}) => {
+const Card = ({name, description, image, width=200, height=260}) => {
   return (
     <div className="col-xl-3 col-sm-6 mb-5">
       <div className="rounded shadow py-5 px-4" style={{height: 450+'px'}}>
-        <img src={image} alt={name} style={{width: 200+'px', height: 260+'px'}}/>
+        <img src={image} alt={name} style={{width: `${width}px`, height: `${height}px`}}/>
         <div >
           <h5 className="text-capitalize">{name}</h5>
           <p>{description}</p>
