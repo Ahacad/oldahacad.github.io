6e08d5d7ec48bc2d6556e6529a7ebc5a888b7cbf
ahacad <1393345075@qq.com>
2021-01-23 22:18:37 +0800

    add section music

---
+++ posts/ACM/Winter contest-3.md
@@ -0,0 +1,14 @@
+---
+title: "Winter Contest 2"
+date: "2021-01-18 17:09"
+tags: ["acm"]
+published: false
+description: "2020 Jiangsu Collegiate Programming Contest"
+---
+
+difficulty:
+```
+tier 1: CDHJ
+tier 2: GIKA
+```
+
---
+++ src/pages/music.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+
+import music from "./music.json";
+import Card from './card.js';
+
+
+const MusicYear = ({year, className="bg-primary"}) => {
+  return (
+    <>
+      <div className={className+" mt-4 ml-1 w-75 text-white rounded"} style={{opacity: "0.8"}}>
+        <h3 className="p-2" style={{fontFamily: "sans-serif"}}>{year}</h3>
+      </div>
+      <div className="container pt-5"> 
+        <div className="row text-center">
+      {
+        music.map((data, index) => {
+          if (data.year == year) {
+            return <Card name={data.name} image={`/music/${data.image}`} description={data.description} width={200} height={200}/>
+          }
+        })
+      }
+        </div>
+      </div> 
+    </>
+  )
+}
+
+const Videos = ({}) => {
+  return (
+    <>
+    <MusicYear year="2021" className="bg-info" />
+  </>
+  )
+}
+
+export default Videos;
