b208861ab5208070775e8f812e66eae41b82f4ab
ahacad <1393345075@qq.com>
2021-02-23 15:46:50 +0800

    feat(sidebar): spinning image & prettify code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 
-
 import MobileSocialLinks from "./MobileSocialLinks"
 import MobilePageLinks from "./MobilePageLinks"
 import SocialLinks from "./SocialLinks"
@@ -10,18 +9,18 @@ import MobileBio from "./MobileBio"
 import "./header.css"
 
 const Header = ({ siteTitle, tagline, author, contacts }) => {
-
   return (
     <header
       className="head-main"
       style={{
-        background: `black`
+        background: `black`,
       }}
     >
-      <div className="head-elements"
+      <div
+        className="head-elements"
         style={{
           margin: `0`,
-          padding: `.75rem`
+          padding: `.75rem`,
         }}
       >
         <h1 className="head-logo ml-4" style={{ margin: 0 }}>
--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -4,14 +4,18 @@ import "./sidebar.css"
 import ahacad from "../../images/ahacad.jpg"
 
 const Bio = ({ author, tagline }) => {
-
-    return (
-        <div className="bio-main w-75">
-            <img src={ahacad} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
-            <h3 className="mt-2 author-bio">{author}</h3>
-            <small className="text-muted">{tagline}</small>
-        </div>
-    )
+  return (
+    <div className="bio-main w-75">
+      <img
+        src={ahacad}
+        style={{ maxWidth: `100px` }}
+        className="profile-img"
+        alt=""
+      />
+      <h3 className="mt-2 author-bio">{author}</h3>
+      <small className="text-muted">{tagline}</small>
+    </div>
+  )
 }
 
 export default Bio
